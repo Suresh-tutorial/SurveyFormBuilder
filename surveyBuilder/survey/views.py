edd2a926f915815199d3c8b757d88aa652f78a57
Suresh-tutorial <sureshshanmugam1501@gmail.com>
2025-06-12 11:55:40 +0530

    add needed files

--- surveyBuilder/survey/views.py
+++ surveyBuilder/survey/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
+def home(request):
+    return render(request,'home.html')
 # Create your views here.
+def send(request):
+    Fn=request.GET['text1']
+    Ln=request.GET['text2']
+    Na=[Fn,Ln]
+    return render(request,'send.html', {'Nae':list(Na)})
